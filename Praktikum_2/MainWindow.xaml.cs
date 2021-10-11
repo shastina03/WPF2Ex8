9c1ee1220b96b20cf3e8d340d828a7484c341c6f
shastina03 <73755402+shastina03@users.noreply.github.com>
2021-10-12 01:49:59 +0300

    Добавьте файлы проекта.

---
+++ Praktikum_2/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Praktikum_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void inkCanvas_Gesture(object sender, InkCanvasGestureEventArgs e)
+        {
+            String gestures = "";
+            // Выборка "предпологаемых" гестур.
+            foreach (GestureRecognitionResult res in e.GetGestureRecognitionResults())
+                gestures += res.ApplicationGesture.ToString() + "; ";
+            // Отображаем название гестуры.
+            gestureName.Text = gestures;
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Выборка всех режимов редактирования InkCanvas.
+            foreach (InkCanvasEditingMode mode in
+           Enum.GetValues(typeof(InkCanvasEditingMode)))
+                lstEditingMode.Items.Add(mode);
+            lstEditingMode.SelectedItem = inkCanvas.EditingMode;
+        }
+        private void lstEditingMode_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+        }
+    }
+}
